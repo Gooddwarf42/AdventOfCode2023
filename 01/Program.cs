3051729e65b1d72891aa701766fbfa70416ea517
marco girardi <marcogirarditesero@gmail.com>
2023-12-07 23:18:27 +0000

    01 part 2 solved

--- 01/Program.cs
+++ 01/Program.cs
@@ -2,18 +2,37 @@
 
 internal class Program
 {
+    private static readonly Dictionary<string, int> DigitMapping = new()
+    {
+        {"zero", 0},
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9},
+    };
+
     private static void Main(string[] args)
     {
         const string inputPath = "/home/marco/share/AdventOfCode2023/01/input";
 
         var inputLines = new List<string>();
-        var digitRegex = new Regex(@"\d");
+
+        var validDigitPatterns = DigitMapping.Keys.ToList();
+        validDigitPatterns.Add(@"\d");
+        var digitRegex = new Regex(string.Join("|", validDigitPatterns));
+
+        System.Console.WriteLine(digitRegex.ToString());
 
         using var streamReader = new StreamReader(inputPath);
         while (!streamReader.EndOfStream)
         {
             var line = streamReader.ReadLine()!;
-            System.Console.WriteLine(line);
+            // System.Console.WriteLine(line);
             inputLines.Add(line);
         }
 
@@ -21,16 +40,21 @@ internal class Program
 
         foreach (var line in inputLines)
         {
-            var matches = digitRegex.Matches(line);
-            if (matches.Count == 0)
+            var matches = Regex.Matches(line, digitRegex.ToString());
+            var matchesRightToLeft = Regex.Matches(line, digitRegex.ToString(), RegexOptions.RightToLeft);
+            if (matches.Count == 0 || matchesRightToLeft.Count == 0)
             {
                 throw new Exception("Occazzo");
             }
 
-            var numberToAdd = int.Parse(matches.First().Value) * 10 + int.Parse(matches.Last().Value);
+            var numberToAdd = ParseDigitFigoMaAncheNo(matches.First().Value) * 10 + ParseDigitFigoMaAncheNo(matchesRightToLeft.First().Value);
             total += numberToAdd;
         }
 
         System.Console.WriteLine(total);
     }
+
+    private static int ParseDigitFigoMaAncheNo(string input)
+        => DigitMapping.ContainsKey(input) ? DigitMapping[input] : int.Parse(input);
+
 }
