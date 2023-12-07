d5d01ac4a623f0363b0d0505b15369f79d4e6c55
marco girardi <marcogirarditesero@gmail.com>
2023-12-07 23:27:24 +0000

    initializing 02

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int day = 2;
+        var inputPath = $"/home/marco/share/AdventOfCode2023/{day:D2}/input";
+        using var streamReader = new StreamReader(inputPath);
+        System.Console.WriteLine("gigi petot");
+    }
+}
