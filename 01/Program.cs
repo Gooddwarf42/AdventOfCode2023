0fcba88072dde9b302b1b66c26b2eb7b7b2ed7e7
marco girardi <marcogirarditesero@gmail.com>
2023-12-07 22:35:45 +0000

    01 part1

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,7 +1,36 @@
-﻿internal class Program
+﻿using System.Text.RegularExpressions;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Gigi petòt!");
+        const string inputPath = "/home/marco/share/AdventOfCode2023/01/input";
+
+        var inputLines = new List<string>();
+        var digitRegex = new Regex(@"\d");
+
+        using var streamReader = new StreamReader(inputPath);
+        while (!streamReader.EndOfStream)
+        {
+            var line = streamReader.ReadLine()!;
+            System.Console.WriteLine(line);
+            inputLines.Add(line);
+        }
+
+        var total = 0;
+
+        foreach (var line in inputLines)
+        {
+            var matches = digitRegex.Matches(line);
+            if (matches.Count == 0)
+            {
+                throw new Exception("Occazzo");
+            }
+
+            var numberToAdd = int.Parse(matches.First().Value) * 10 + int.Parse(matches.Last().Value);
+            total += numberToAdd;
+        }
+
+        System.Console.WriteLine(total);
     }
 }
