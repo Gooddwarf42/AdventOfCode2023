c3f221597f893b3455c91e5e2b660d0af77fc489
marco girardi <marcogirarditesero@gmail.com>
2023-12-08 11:08:18 +0000

    input

--- 03/Program.cs
+++ 03/Program.cs
@@ -5,14 +5,32 @@
         const int day = 3;
         var inputPath = $"/home/marco/share/AdventOfCode2023/{day:D2}/input";
         using var streamReader = new StreamReader(inputPath);
-        
 
-        var inputPaddd = new  List<char[]>();
-        
+        var inputPadded = GetPaddedInput(streamReader);
+
+        // foreach (var line in inputPadded)
+        // {
+        //     Console.WriteLine(string.Join(null, line));
+        // }
+
+    }
+
+    private static char[][] GetPaddedInput(StreamReader streamReader)
+    {
+        var inputPadded = new List<char[]>();
 
         while (!streamReader.EndOfStream)
         {
-
+            var line = streamReader.ReadLine()!;
+            inputPadded.Add($".{line}.".ToCharArray());
         }
+
+        var lineLength = inputPadded[0].Length;
+        var paddingLine = new char[lineLength];
+        Array.Fill(paddingLine, '.');
+        inputPadded.Insert(0, paddingLine);
+        inputPadded.Add(paddingLine);
+
+        return inputPadded.ToArray();
     }
 }
