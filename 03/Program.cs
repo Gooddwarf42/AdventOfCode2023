463c852828e2eb1140da63c45c7ac43e05c29024
marco girardi <marcogirarditesero@gmail.com>
2023-12-08 10:46:53 +0000

    started

---
+++ 03/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int day = 3;
+        var inputPath = $"/home/marco/share/AdventOfCode2023/{day:D2}/input";
+        using var streamReader = new StreamReader(inputPath);
+        
+
+        var inputPaddd = new  List<char[]>();
+        
+
+        while (!streamReader.EndOfStream)
+        {
+
+        }
+    }
+}
