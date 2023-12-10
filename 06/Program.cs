af7f30a0181084d4ada34b80978d29f4d02031a0
marco girardi <marcogirarditesero@gmail.com>
2023-12-10 11:46:38 +0000

    part1

--- 06/Program.cs
+++ 06/Program.cs
@@ -9,6 +9,48 @@ internal class Program
         const int day = 6;
         var inputPath = $"/home/marco/share/AdventOfCode2023/{day:D2}/input";
         using var streamReader = new StreamReader(inputPath);
-        
+        var times = streamReader.ReadLine()!.GetNumbers().ToArray();
+        var distances = streamReader.ReadLine()!.GetNumbers().ToArray();
+
+        var result = 1L;
+        for (int i = 0; i < times.Length; i++)
+        {
+            result *= GetNumberOfWaysToWin(times[i], distances[i]);
+        }
+        System.Console.WriteLine(result);
     }
+
+    private static long GetNumberOfWaysToWin(int time, int distanceToBeat)
+    {
+        var buttonTime = time / 2;
+        var intervalSize = time / 2;
+        var isLastButtonTimeWin = true;
+
+        while (intervalSize != 0)
+        {
+            var distanceTraveled = buttonTime * (time - buttonTime);
+            isLastButtonTimeWin = distanceTraveled > distanceToBeat;
+
+            intervalSize /= 2;
+            buttonTime = isLastButtonTimeWin ? buttonTime - intervalSize : buttonTime + intervalSize;
+        }
+
+        //inelegant adjustment
+        if (!isLastButtonTimeWin)
+        {
+            buttonTime++;
+        }
+
+        //here buttonTime is the minimum time to press the button to win. Due to simmetry we can say that
+        return (time + 1) - 2 * buttonTime;
+    }
+}
+
+internal static class StringExtensions
+{
+    private static readonly Regex NumberRegex = new(@"\d+");
+
+    public static IEnumerable<int> GetNumbers(this string s) =>
+        NumberRegex.Matches(s)
+            .Select(m => int.Parse(m.Value));
 }
