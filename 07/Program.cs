81ead52b597f2bc5aff7b8b42187272e46a3ffeb
marco girardi <marcogirarditesero@gmail.com>
2023-12-10 14:53:38 +0000

    init

---
+++ 07/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static readonly Regex NumberRegex = new(@"\d+");
+
+    private static void Main(string[] args)
+    {
+        const int day = 7;
+        var inputPath = $"/home/marco/share/AdventOfCode2023/{day:D2}/input";
+        using var streamReader = new StreamReader(inputPath);
+    }
+}
