cd0576e929218c68bfe20f99f92cb3354d127b3d
marco girardi <marcogirarditesero@gmail.com>
2023-12-12 21:21:46 +0000

    init

---
+++ 09/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var inputPath = $"input.txt";
+        using var streamReader = new StreamReader(inputPath);
+        
+        System.Console.WriteLine("mimmo");
+    }
+
+    
+}
+
+
