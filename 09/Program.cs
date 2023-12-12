8bc430111965b47d13a1dafa8177983e1199897b
marco girardi <marcogirarditesero@gmail.com>
2023-12-12 21:58:50 +0000

    Pascal triangle

--- 09/Program.cs
+++ 09/Program.cs
@@ -8,11 +8,51 @@ internal class Program
     {
         var inputPath = $"input.txt";
         using var streamReader = new StreamReader(inputPath);
-        
-        System.Console.WriteLine("mimmo");
+
+        System.Console.WriteLine($"Testing 50 choose 25: {MathUtilities.Binom(50, 25)}");
+
+        MathUtilities.PrintComputedBinoms();
     }
 
-    
+
 }
 
 
+internal static class MathUtilities
+{
+    private static List<long[]> _binom = [[1]];
+    public static long Binom(int n, int k)
+    {
+        if (k < 0 || k > n)
+        {
+            return 0;
+        }
+
+        if (BinomAlreadyComputed(n, k))
+        {
+            return _binom[n][k];
+        }
+
+        var result = Binom(n - 1, k - 1) + Binom(n - 1, k);
+
+        //if previous rows are needed they were computed by previous recursive calls!
+        if (_binom.Count <= n)
+        {
+            _binom.Add(new long[n + 1]);
+        }
+        _binom[n][k] = result;
+        return result;
+    }
+
+    public static void PrintComputedBinoms()
+    {
+        foreach (var row in _binom)
+        {
+            System.Console.WriteLine(string.Join(' ', row));
+        }
+    }
+
+    private static bool BinomAlreadyComputed(int n, int k) =>
+        _binom.Count > n
+        && _binom[n][k] != 0;
+}
