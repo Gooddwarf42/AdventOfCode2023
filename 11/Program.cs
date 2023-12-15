dafdd625df41d86079b281cdb159e668932bb582
Marco Girardi <marco.girardi@factorymind.com>
2023-12-15 09:18:51 +0100

    init

---
+++ 11/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    
+
+    public static void Main(string[] args)
+    {
+        const string inputPath = "input.txt";
+        using var streamReader = new StreamReader(inputPath);
+    }
+}
