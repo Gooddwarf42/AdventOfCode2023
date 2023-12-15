5e54bde4ab6bf59e69219ddbfb51477be76c96db
Marco Girardi <marco.girardi@factorymind.com>
2023-12-15 09:42:20 +0100

    WIP

--- 11/Program.cs
+++ 11/Program.cs
@@ -1,10 +1,23 @@
 ﻿internal class Program
 {
-    
-
     public static void Main(string[] args)
     {
         const string inputPath = "input.txt";
         using var streamReader = new StreamReader(inputPath);
+        var universe = ParseInput(streamReader).ToArray();
+        var rowsToExpand = universe
+            .Select((r, i) => (Row: r, Index: i))
+            .Where(tuple => tuple.Row.ToCharArray().All(c => c == '.'))
+            .Select(tuple => tuple.Index);
+        var columnsToExpand = //TODO;
+
+    }
+
+    private static IEnumerable<string> ParseInput(StreamReader streamReader)
+    {
+        while (!streamReader.EndOfStream)
+        {
+            yield return streamReader.ReadLine()!;
+        }
     }
 }
