05f79e220972a860f198ef3bf2bc566fda157dc4
marco girardi <marcogirarditesero@gmail.com>
2023-12-16 15:28:26 +0000

    input

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Linq.Expressions;
+
 internal class Program
 {
     private static void Main(string[] args)
@@ -6,14 +8,39 @@ internal class Program
         const string inputPath = "input.txt";
         using var streamReader = new StreamReader(inputPath);
         var input = ParseInput(streamReader);
-        
+
+        foreach(var puzzle in input){
+            System.Console.WriteLine(puzzle);
+        }
+
     }
 
-    private static IEnumerable<char[]> ParseInput(StreamReader streamReader)
+    private static IEnumerable<Picross> ParseInput(StreamReader streamReader)
     {
         while (!streamReader.EndOfStream)
         {
-            yield return streamReader.ReadLine()!.ToCharArray();
+            var line = streamReader.ReadLine()!;
+            var lineSplit = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            yield return new Picross
+            {
+                Schema = lineSplit[0].ToCharArray().ToList(),
+                Constraints = lineSplit[1].Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                    .Select(s => int.Parse(s))
+                    .ToList(),
+            };
         }
+    }
+}
+
+internal sealed class Picross
+{
+    public required List<char> Schema { get; set; }
+    public required List<int> Constraints { get; set; }
+
+    public override string ToString()
+    {
+        var schema = string.Join(null, Schema);
+        var constraints = string.Join(null, Constraints);
+        return $"{schema} \t {constraints}";
     }
 }
