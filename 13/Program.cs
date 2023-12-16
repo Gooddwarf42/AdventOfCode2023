8cac5211e0f2ef0b6115bfcf48872cf5b236dfaf
marco girardi <marcogirarditesero@gmail.com>
2023-12-16 22:57:43 +0000

    init

---
+++ 13/Program.cs
@@ -0,0 +1,19 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string inputPath = "input.txt";
+        using var streamReader = new StreamReader(inputPath);
+        var input = ParseInput(streamReader);
+        
+    }
+
+    private static IEnumerable<char[]> ParseInput(StreamReader streamReader)
+    {
+        while (!streamReader.EndOfStream)
+        {
+            yield return streamReader.ReadLine()!.ToCharArray();
+        }
+    }
+}
