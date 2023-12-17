54945a8239b9544eb0df21cbf0bacf8ff52076f9
marco girardi <marcogirarditesero@gmail.com>
2023-12-17 20:59:17 +0000

    init

---
+++ 14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+
+
+        sw.Stop();
+        System.Console.WriteLine($"Time: {sw.ElapsedMilliseconds}");
+
+
+    }
+}
