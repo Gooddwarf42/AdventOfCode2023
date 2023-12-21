c07184bbc285b05b3413e4737e52deaac083a5a7
Marco Girardi <marco.girardi@factorymind.com>
2023-12-21 13:08:43 +0100

    init

---
+++ 16/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+
+        const string inputPath = "input.txt";
+        using var streamReader = new StreamReader(inputPath);
+
+        var inputSequence = ParseInput(streamReader).ToArray();
+
+        sw.Stop();
+        System.Console.WriteLine($"Time: {sw.ElapsedMilliseconds}");
+
+        System.Console.WriteLine(42);
+    }
+
+    private static char[][] ParseInput(StreamReader streamReader)
+    {
+        throw new NotImplementedException();
+    }
+}
